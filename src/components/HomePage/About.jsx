475564afc9337dd7f255e87febb694750c4695da
munir <smsirajulmonir@gmail.com>
2024-06-25 00:39:57 +0600

    about banner navbar added

---
+++ src/components/HomePage/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='text-slate-900'>
+            <div className='container mx-auto'>
+
+            </div>
+        </div>
+    );
+};
+
+export default About;
